4957294131ab7c3011021616a6b70138009f30b5
Paul Hebert <paul@cloudfour.com>
2023-02-21 19:02:50 -0800

    Fleshing out features and docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ output: "static" });
+export default defineConfig({
+  srcDir: "./docs",
+  outDir: "./docs-dist",
+  output: "static",
+});
--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,13 @@ interface SeededOptions {
   precision: number;
 }
 
+interface HslOptions {
+  h?: number[] | number;
+  s?: number[] | number;
+  l?: number[] | number;
+  a?: number[] | number | null;
+}
+
 const defaultOptions = { precision: 2 };
 
 export function seeded(seed?: number, options?: SeededOptions) {
@@ -18,22 +25,56 @@ export function seeded(seed?: number, options?: SeededOptions) {
     ...(options || {}),
   };
 
-  const randomDecimal = () => seededRandom.floatBetween(0, 1);
+  const randomDecimal = seededRandom.random;
 
-  const newSeed = (min: number, max: number) =>
+  const newSeed = (min?: number, max?: number) =>
     roundToPrecision(randomDecimal() * (max - min) + min, precision);
 
+  // Math
+  newSeed.int = seededRandom.intBetween;
   newSeed.decimal = () => roundToPrecision(randomDecimal(), precision);
-  newSeed.percent = () => roundToPrecision(randomDecimal() * 100, precision);
+  newSeed.percent = (min = 0, max = 100) =>
+    roundToPrecision(seededRandom.floatBetween(min, max), precision);
 
-  newSeed.hue = () =>
-    roundToPrecision(seededRandom.floatBetween(0, 360), precision);
+  // Geometry
+  newSeed.degree = (min = 0, max = 360) =>
+    roundToPrecision(seededRandom.floatBetween(min, max), precision);
+  // TODO: Points
+
+  // Colors
+  newSeed.hue = newSeed.degree;
   newSeed.saturation = newSeed.percent;
   newSeed.lightness = newSeed.percent;
   newSeed.alpha = newSeed.percent;
 
-  newSeed.hsl = () =>
-    `hsl(${newSeed.hue()}, ${newSeed.saturation()}%, ${newSeed.lightness()}%)`;
+  const numberOrRangeToNumber = (val: number[] | number) =>
+    isNaN(val as number) ? newSeed(...(val as number[])) : val;
+
+  const defaultHslOptions: HslOptions = {
+    h: [0, 360],
+    s: [0, 100],
+    l: [0, 100],
+    a: null,
+  };
+
+  newSeed.hsl = (settings?: HslOptions) => {
+    const { h, s, l, a } = {
+      ...defaultHslOptions,
+      ...(settings || {}),
+    };
+
+    const hue = numberOrRangeToNumber(h);
+    const sat = numberOrRangeToNumber(s);
+    const light = numberOrRangeToNumber(l);
+    const alpha = a ? numberOrRangeToNumber(a) : null;
+
+    return `hsl(${hue}, ${sat}%, ${light}% ${alpha ? `${alpha}%` : ""})`;
+  };
+
+  // Chance
+  newSeed.chance = (chance = 0.5) => seededRandom.floatBetween(0, 1) < chance;
+  newSeed.itemInArray = (array: any[]) =>
+    array[newSeed.int(0, array.length - 1)];
 
   return newSeed;
 }
