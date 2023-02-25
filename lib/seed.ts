69dd24bc15787f4d20b8dbd638545e1cbd76fe51
Paul Hebert <paul@cloudfour.com>
2023-02-25 13:03:30 -0800

    Fix seed helper

--- lib/seed.ts
+++ lib/seed.ts
@@ -4,4 +4,4 @@ import { generateHelpers } from "./generate-helpers";
 const { create } = randomSeed;
 
 // TODO: Allow seed strings
-export const seed = (seed: number) => generateHelpers(create(seed));
+export const seed = (seed: number) => generateHelpers(create(seed).random);
