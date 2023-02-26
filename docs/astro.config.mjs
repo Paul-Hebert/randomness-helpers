690405f437f1d849b88a2e479dd01226c225a697
Paul Hebert <paul@cloudfour.com>
2023-02-26 10:18:10 -0800

    Move astro stuff to its own docs folder

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  srcDir: "./docs",
-  outDir: "./docs-dist",
+  srcDir: "./",
+  outDir: "../docs-dist",
   output: "static",
 });
