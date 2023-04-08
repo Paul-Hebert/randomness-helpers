0adf068e274bcaf8d2c5c135d523a43eac07c3f9
Paul Hebert <paul@cloudfour.com>
2023-04-08 10:14:26 -0700

    Splitting docs and examples

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<header>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/docs">Docs</a>
+    <a href="/examples">Examples</a>
+  </nav>
+</header>
+
+<slot />
+
+<footer>Made by <a href="https://paulmh.xyz">Paul Hebert</a></footer>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: end;
+    gap: 1em;
+  }
+
+  nav > a:first-child {
+    margin-right: auto;
+  }
+</style>
