608b9dba51a3db7c78c195580bf17cc1a05ba283
Paul Hebert <paul@cloudfour.com>
2023-05-11 13:55:21 -0700

    Circles example

---
+++ docs/src/examples/circles.ts
@@ -0,0 +1,40 @@
+// @ts-nocheck
+import type { ExampleData } from "./example-data";
+import {
+  random,
+  randomInt,
+  randomDegree,
+  randomHsl,
+} from "../../../dist/index";
+
+export const circles: ExampleData = {
+  title: "Circles",
+  slug: "circles",
+  imports: ["random", "randomHsl", "randomDegree"],
+  callback: () => {
+    const gridSize = 100;
+
+    const circle = ({ x = 50, y = 50, radius = 5, fill = "#fff" }) => `
+      <circle cx="${x}" cy="${y}" r="${radius}" fill="${fill}" />
+    `;
+
+    const circles = [];
+
+    for (let i = 0; i < randomInt(20, 100); i++) {
+      circles.push(
+        circle({
+          x: random(0, gridSize),
+          y: random(0, gridSize),
+          radius: random(2, 5),
+          fill: randomHsl(),
+        })
+      );
+    }
+
+    return `
+      <svg viewBox="0 0 100 100" width="300" height="300" style="background: #000">
+        ${circles.join("\n")}
+      </svg>
+    `;
+  },
+};
--- docs/src/examples/index.ts
+++ docs/src/examples/index.ts
@@ -2,5 +2,12 @@ import { colors } from "./colors";
 import { snowFlakes } from "./snow-flakes";
 import { solarSystem } from "./solar-system";
 import { squares } from "./squares";
+import { circles } from "./circles";
 
-export const examples = [solarSystem, colors, squares, snowFlakes];
+export const examples = [
+  solarSystem,
+  colors,
+  squares,
+  snowFlakes,
+  // circles
+];
