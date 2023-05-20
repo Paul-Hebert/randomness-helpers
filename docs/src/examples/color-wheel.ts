40fa231b78f82990af29ae7f0cd8dcb16e7c8969
Paul Hebert <paul@cloudfour.com>
2023-05-20 11:16:24 -0700

    Another Example

---
+++ docs/src/examples/color-wheel.ts
@@ -0,0 +1,44 @@
+// @ts-nocheck
+import type { ExampleData } from "./example-data";
+import {
+  random,
+  randomInt,
+  randomDegree,
+  randomHsl,
+} from "../../../dist/index";
+
+export const colorWheel: ExampleData = {
+  title: "Color Wheel",
+  slug: "color-wheel",
+  imports: ["random", "randomHsl", "randomDegree"],
+  callback: () => {
+    const gridSize = 100;
+
+    const circle = ({ x = 50, y = 50, radius = 5, fill = "#fff" }) => `
+      <circle cx="${x}" cy="${y}" r="${radius}" fill="${fill}" />
+    `;
+
+    const circles = [];
+
+    for (let i = 0; i < 200; i++) {
+      const angle = randomDegree();
+      const distance = random(1, 70);
+      const angleInRadians = (angle * Math.PI) / 180;
+
+      circles.push(
+        circle({
+          x: 50 + Math.cos(angleInRadians) * distance,
+          y: 50 + Math.sin(angleInRadians) * distance,
+          radius: 5 - distance / 15,
+          fill: randomHsl({ h: angle, l: 25 + distance, s: [30, 70] }),
+        })
+      );
+    }
+
+    return `
+      <svg viewBox="0 0 100 100" width="300" height="300" style="background: #000">
+        ${circles.join("\n")}
+      </svg>
+    `;
+  },
+};
--- docs/src/examples/index.ts
+++ docs/src/examples/index.ts
@@ -3,11 +3,13 @@ import { snowFlakes } from "./snow-flakes";
 import { solarSystem } from "./solar-system";
 import { squares } from "./squares";
 import { circles } from "./circles";
+import { colorWheel } from "./color-wheel";
 
 export const examples = [
   solarSystem,
   colors,
   squares,
   snowFlakes,
-  // circles
+  circles,
+  colorWheel,
 ];
